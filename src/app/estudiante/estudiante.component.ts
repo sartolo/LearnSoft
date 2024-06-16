a1384e31e1a22d90fca6d867dcf25668fee05116
Etiene Chaves <sartolo8@hotmail.com>
2024-06-16 12:26:10 -0600

    Creacion de componentes Curso, Profesor, Estudiante y Admin

---
+++ src/app/estudiante/estudiante.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estudiante',
+  standalone: true,
+  imports: [],
+  templateUrl: './estudiante.component.html',
+  styleUrl: './estudiante.component.css'
+})
+export class EstudianteComponent {
+
+}
